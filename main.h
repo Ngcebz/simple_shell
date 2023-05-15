11972bf16743e2c8dba44423a7c51bd7b59920e9
Nongcebo <nssbmcnamarabold@yahoo.com>
2023-05-15 01:11:45 -0700

    updated main.h

--- main.h
+++ main.h
@@ -8,6 +8,9 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 
+#define MAX_INPUT_LENGTH 1024
+#define MAX_NUM_ARGS 128
+
 void print_prompt(void);
 ssize_t read_input(char *input);
 void execute_command(char *command);
@@ -15,5 +18,7 @@ void execute_command(char **args);
 void wait_for_child(pid_t pid, int *status);
 void handle_command_execution(char **args);
 void parse_command(char *input, char **args);
+int check_command_exists(char *command);
+ssize_t read_command(char *input);
 
 #endif /* MAIN_H */
