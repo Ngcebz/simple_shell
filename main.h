e341a1ce0c468d1a679a03516a3cfeb7c6213ed0
Nongcebo <nssbmcnamarabold@yahoo.com>
2023-05-14 23:27:22 -0700

    args.c

--- main.h
+++ main.h
@@ -11,5 +11,9 @@
 void print_prompt(void);
 ssize_t read_input(char *input);
 void execute_command(char *command);
+void execute_command(char **args);
+void wait_for_child(pid_t pid, int *status);
+void handle_command_execution(char **args);
+void parse_command(char *input, char **args);
 
 #endif /* MAIN_H */
